4385289df2fd0c8649cddd5d3634eee992b46645
CuongDangCode <155418798+CuongDangCode@users.noreply.github.com>
2024-01-24 22:16:24 +0700

    Up Normalize Text

---
+++ NormalizeText/src/View/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package View;
+import Controller.Controller;
+import Model.Text;
+/**
+ *
+ * @author ROG
+ */
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Text text = new Text();
+        new Controller(text).run();
+    }
+    
+}
